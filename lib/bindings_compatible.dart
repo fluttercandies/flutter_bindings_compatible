ea9c5ac8412c55d63f8d566275b5b310cb1b8d5b
Alex Li <github@alexv525.com>
2023-02-07 16:01:48 +0800

    🚨 Globally ignore `unnecessary_cast`

--- lib/bindings_compatible.dart
+++ lib/bindings_compatible.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a MIT license that can be found
 // in the LICENSE file.
 
-// ignore_for_file: unnecessary_cast
-
 /// Provides compatible bindings instance across different Flutter version.
 library bindings_compatible;
 
